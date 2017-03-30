3540b13a5072b9b49eceeee59503d4024ffc2355
clairehxq <clairehxq@users.noreply.github.com>
2017-03-30 14:33:06 -0400

    add gaussian filter
    smoothing on specified axis

--- py/gaussian_filter.py
+++ py/gaussian_filter.py
@@ -9,7 +9,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from scipy.ndimage.filters import gaussian_filter as gf
 
-temp = np.array(map(lambda x: 63 - x, np.load('/Users/macbook/GH/cira/data/IR_temperature.npy')))
+temp = np.array(map(lambda x: 63 - x, np.load('/data/IR_temperature.npy')))
 # record original shape
 shape = temp.shape
 def transfer(temp):
@@ -18,15 +18,15 @@ def transfer(temp):
 
 temp1 = transfer(temp)
 
-frame = np.load('/Users/macbook/GH/cira/data/IRTL_frames/IRTL_frame_0280.npy')
+frame = np.load('/data/IRTL_frames/IRTL_frame_0280.npy')
 
 def transferback(ft):
     return ft.T.reshape(shape)
 
 res = {}
 tem = {}
-for sigma in [1,2,3,4,5]:
-    tem[sigma] = gf(temp1,sigma)
+for sigma in [2,3,4,5]:
+    tem[sigma] = gf(temp1,[sigma,0])
     # take a subtraction of filtered data by true temperature
     
     # each res[] shapes [696960, 708]
